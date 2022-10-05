d16c45c20bc997581b2a33ecadc56c180fa60dda
Deti Baholli <detibaholli@gmail.com>
2022-10-05 03:05:51 +0200

    Add File Structure, Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
-import './App.css';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  const [content, setContent] = useState('Not fetched');
-  const [input, setInput] = useState('');
-  const [content2, setContent2] = useState('Not requested');
+import './App.scss';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
 
-  console.log(process.env.REACT_APP_API);
 
-  const changeInput = (text) => {
-    setInput((oldtext) => (oldtext = text));
-  };
-
-  const sendRequest = () => {
-    console.log(`${process.env.REACT_APP_API}/controller/${input}`);
-    axios.get(`${process.env.REACT_APP_API}/controller/${input}`).then((res) => {
-      setContent2(res.data.request);
-    });
-  };
-
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API}/controller`).then((res) => {
-      setContent(res.data.content);
-    });
-  }, []);
-
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Elefanti Video - Test Change</h1>
-      <p>{content}</p>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input onInput={(e) => changeInput(e.target.value)} type='text' />
-        <button onClick={sendRequest} type='submit'>
-          Submit
-        </button>
-      </form>
-      <h5>{content2}</h5>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<div>404</div>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
