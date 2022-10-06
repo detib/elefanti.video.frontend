b65ea8561d5dfb878a9137b8d320efc1700699cf
Harisa <hb51339@ubt-uni.net>
2022-10-07 01:30:55 +0200

    Login Design

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import SignIn from './Pages/SignIn/SignIn.js';
 
 
 const App = () => {
   return (
+  
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
         <Route path='*' element={<div>404</div>} />
+        
       </Routes>
     </Router>
   );
---
+++ src/Pages/SignIn/SignIn.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SignInStyle.css';
+import logo from '../../img/logo.png';
+
+
+const SignIn =()=>{
+    return(
+        <div>
+            <form autoComplete='off' className="formFields" >
+                <div className="formField">
+                     <div className='logoContainer'>
+                        <img src={logo} className='logo'/>
+                     </div>
+
+                        
+                    <div className='formInput'>
+                             <h1 className='loginTitle'>Login</h1>
+                            <input type='email' placeholder='Enter your email..'/>
+                            <input type='password' placeholder='Enter your Password..'/>
+                            <button>Login</button>    
+                    </div> 
+                    
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default SignIn;
