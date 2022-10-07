0920647002ac3da55d90af15933e7dbfa3270df8
Harisa <hb51339@ubt-uni.net>
2022-10-07 22:39:46 +0200

    SignUp Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import SignUp from './components/pages/SignUp';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route path='*' element={<div>404</div>} />
       </Routes>
     </Router>
