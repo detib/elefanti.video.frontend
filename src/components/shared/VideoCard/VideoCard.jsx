fe08cf9c38c0abb979c29bce098b01caeef7453b
Deti Baholli <detibaholli@gmail.com>
2022-10-14 20:11:23 +0200

    Updated Video Card

--- src/components/shared/VideoCard/VideoCard.jsx
+++ src/components/shared/VideoCard/VideoCard.jsx
@@ -1,12 +1,19 @@
 import './VideoCard.scss';
 import '../styles/shared.scss';
 import { Link } from 'react-router-dom';
+import { formatDistance } from 'date-fns'
 
 const VideoCard = (props) => {
+  const date = new Date(props.createdOn)
+  const VideoDateCreated = formatDistance(date, new Date());
+  let description = props.description;
+  if(description.length > 65) {
+    description = props.description.substring(0, 64) + "...";
+  }
   return (
     <Link to={props.to}>
       <div className='card'>
-        <div className='card__actionArea'>
+        <div className='card__actionArea flex-col'>
           <div className='card__media'>
             <img
               className='card__image'
@@ -14,11 +21,10 @@ const VideoCard = (props) => {
               alt='thumbnail'
             />
           </div>
-          <div className='card__content'>
+          <div className='card__content flex-col'>
             <h2 className='card__title'>{props.title}</h2>
-            <p className='card__description'>{props.description}</p>
-            <p className='card__timestamp'>{props.timestamp}</p>
-            <p className='card__createdOn'>{props.createdOn}</p>
+            <p className='card__description'>{description}</p>
+            <p className='card__createdOn'>{VideoDateCreated} ago</p>
           </div>
         </div>
       </div>
