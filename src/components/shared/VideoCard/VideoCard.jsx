ac3980f637fbfe0d5c647d615e4f8f8e481da0f2
Deti Baholli <detibaholli@gmail.com>
2022-10-16 02:11:23 +0200

    Updated video card, added null checks

--- src/components/shared/VideoCard/VideoCard.jsx
+++ src/components/shared/VideoCard/VideoCard.jsx
@@ -1,30 +1,29 @@
 import './VideoCard.scss';
 import '../styles/shared.scss';
 import { Link } from 'react-router-dom';
-import { formatDistance } from 'date-fns'
+import { formatDistance } from 'date-fns';
 
 const VideoCard = (props) => {
-  const date = new Date(props.createdOn)
-  const VideoDateCreated = formatDistance(date, new Date());
-  let description = props.description;
-  if(description.length > 65) {
-    description = props.description.substring(0, 64) + "...";
+  let VideoDateCreated;
+  if (props.createdOn) {
+    const date = new Date(props.createdOn);
+    VideoDateCreated = formatDistance(date, new Date());
+  }
+  let description;
+  if (props.description && props.description.length > 65) {
+    description = props.description.substring(0, 64) + '...';
   }
   return (
     <Link to={props.to}>
       <div className='card'>
         <div className='card__actionArea flex-col'>
           <div className='card__media'>
-            <img
-              className='card__image'
-              src={`https://img.youtube.com/vi/${props.id}/mqdefault.jpg`}
-              alt='thumbnail'
-            />
+            <img className='card__image' src={`https://img.youtube.com/vi/${props.id}/mqdefault.jpg`} alt='thumbnail' />
           </div>
           <div className='card__content flex-col'>
             <h2 className='card__title'>{props.title}</h2>
-            <p className='card__description'>{description}</p>
-            <p className='card__createdOn'>{VideoDateCreated} ago</p>
+            {description && <p className='card__description'>{description}</p>}
+            {VideoDateCreated && <p className='card__createdOn'>{VideoDateCreated} ago</p>}
           </div>
         </div>
       </div>
