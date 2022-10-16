c872704fa6dc03e1c720db6236370e1d85e00976
Deti Baholli <detibaholli@gmail.com>
2022-10-16 06:56:08 +0200

    useFetch custom hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
+function useFetch(from, take) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [videos, setVideos] = useState([]);
+
+  const fetchVideos = useCallback(async () => {
+    setLoading(true);
+    setError(false);
+    await axios
+      .get(`${process.env.REACT_APP_API}/api/videos?from=${from}&take=${take}`)
+      .then((response) => {
+        console.log(response.data);
+        setVideos((prev) => prev = [...prev, ...response.data]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, [from, take]);
+
+  useEffect(() => {
+    fetchVideos();
+  }, [fetchVideos, from, take]);
+
+  return { loading, error, videos };
+}
+
+export default useFetch;
