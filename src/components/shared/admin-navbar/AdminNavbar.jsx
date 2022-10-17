70fbeebbd0c7da3f9a42b5c5e0de6881675fec17
Deti Baholli <detibaholli@gmail.com>
2022-10-17 02:14:32 +0200

    Updated Admin Navbar

--- src/components/shared/admin-navbar/AdminNavbar.jsx
+++ src/components/shared/admin-navbar/AdminNavbar.jsx
@@ -1,58 +1,54 @@
 import React, { useState } from 'react';
-import { FaTh, FaBars} from "react-icons/fa";
+import { FaTh, FaBars } from 'react-icons/fa';
 import { BsFillPlayCircleFill } from 'react-icons/bs';
 import { BiCategoryAlt } from 'react-icons/bi';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './AdminNavbar.scss';
+import '../styles/shared.scss';
 
+const AdminNavbar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: '',
+      name: 'Dashboard',
+      icon: <FaTh />,
+    },
+    {
+      path: '/video',
+      name: 'Videos',
+      icon: <BsFillPlayCircleFill />,
+    },
+    {
+      path: '',
+      name: 'Categories',
+      icon: <BiCategoryAlt />,
+    },
+  ];
 
-const AdminNavbar = ({children}) => {
-
-    
-    const[isOpen ,setIsOpen] = useState(false);
-    const toggle = () => setIsOpen (!isOpen);
-    const menuItem=[
-        {
-            path:"",
-            name:"Dashboard",
-            icon:<FaTh/>
-        },
-    
-        {
-            path:"/video",
-            name:"Videos",
-            icon:<BsFillPlayCircleFill/>
-        },
-       
-        {
-            path:"",
-            name:"Categories",
-            icon:<BiCategoryAlt/>
-        }
-    ]
-    return (
-        <div className="container">
-           <div style={{width: isOpen ? "300px" : "70px"}} className="sidebar">
-               <div className="top_section">
-                <div></div>
-                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo"> Elefanti Video</h1>
-                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
-                       <FaBars onClick={toggle}/>
-                   </div>
-               </div> <div className='pages'>
-               { 
-                   menuItem.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                           <div className="icon">{item.icon}</div>
-                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
-                       </NavLink>
-                   ))
-                  
-               } </div>
-           </div>
-           <main>{children}</main>
+  return (
+    <div className='admin-nav-container'>
+      <div className={`${isOpen ? 'sidebar-open' : ''} sidebar`}>
+        <div className='top_section inline spread'>
+          <h1 style={{ display: isOpen ? 'block' : 'none' }} className='logo'>
+            Elefanti Video
+          </h1>
+          <div onClick={toggle} className='bars'>
+            <FaBars />
+          </div>
         </div>
-    );
+        <div className='pages flex-col'>
+          {menuItem.map((item, index) => (
+            <Link to={item.path} key={index} className='link inline'>
+              <div className='icon center-item'>{item.icon}</div>
+              <p className='link_text'>{item.name}</p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminNavbar;
