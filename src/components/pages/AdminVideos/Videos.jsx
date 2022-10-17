9dfda5ab5489f4bffdd5bfb104617c02bc6493a8
Deti Baholli <detibaholli@gmail.com>
2022-10-17 02:14:05 +0200

    Add Videos Panel

---
+++ src/components/pages/AdminVideos/Videos.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+import '../../shared/styles/shared.scss';
+import './styles/Videos.scss';
+
+const Videos = () => {
+  return (
+    <div>
+      <h1>Videos</h1>
+    </div>
+  );
+};
+
+export default Videos;
